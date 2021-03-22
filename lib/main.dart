cd02618ba90407b61ad293a82a1561f5e6669494
tri yulianto <threeyulianto333@gmail.com>
2021-03-22 15:40:07 +0700

    btn login

--- lib/main.dart
+++ lib/main.dart
@@ -63,28 +63,58 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Stack(children: <Widget>[
-            SvgPicture.asset("lib/assets/images/login/bg_wave1_biru.svg"),
-            Padding(
-                padding: const EdgeInsets.all(100),
-                child:
-                    (SvgPicture.asset("lib/assets/images/login/img_logo.svg"))),
-          ]),
-          Padding(
-            padding: const EdgeInsets.only(left: 50, right: 50),
-            child: Column(children: <Widget>[
-              TextField(
-                decoration: InputDecoration(hintText: 'Masukkan Email Anda'),
-              ),
-              TextField(
-                decoration: InputDecoration(hintText: 'Masukkan Password Anda'),
-              )
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Stack(children: <Widget>[
+              SvgPicture.asset("lib/assets/images/login/bg_wave1_biru.svg"),
+              Padding(
+                  padding: const EdgeInsets.all(100),
+                  child: (SvgPicture.asset(
+                      "lib/assets/images/login/img_logo.svg"))),
             ]),
-          )
-        ],
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            Padding(
+              padding: const EdgeInsets.only(left: 50, right: 50),
+              child: Column(children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Masukkan Email Anda',
+                      labelText: 'Email',
+                      border: OutlineInputBorder()),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Masukkan Password Anda',
+                        labelText: 'Password',
+                        border: OutlineInputBorder()),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20, bottom: 100),
+                  child: TextButton(
+                      child: Text("Login".toUpperCase(),
+                          style: TextStyle(fontSize: 20)),
+                      style: ButtonStyle(
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                              EdgeInsets.all(15)),
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.green),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ))),
+                      onPressed: () => null),
+                ),
+              ]),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
