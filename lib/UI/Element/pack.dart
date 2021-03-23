07631f2afd0963819af318e5d93a8525c89001bd
tri yulianto <threeyulianto333@gmail.com>
2021-03-23 15:59:29 +0700

    dashboard [⏱]

--- lib/UI/Element/pack.dart
+++ lib/UI/Element/pack.dart
@@ -9,37 +9,33 @@ import 'imgVector.dart';
 Widget kotakTanggal = Container(
   child: Row(children: <Widget>[
     Stack(children: <Widget>[
-      SizedBox(height: 80, child: imgKotakTgl),
-      SizedBox(
-        height: 80,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                  margin: const EdgeInsets.only(right: 20), child: txtTanggal),
-              Container(
-                child: Column(children: <Widget>[
-                  Row(children: <Widget>[
-                    Container(
-                        margin: const EdgeInsets.only(right: 10),
-                        child: imgIcoIn),
-                    txtJM
-                  ]),
-                  Row(children: <Widget>[
-                    Container(
-                        margin: const EdgeInsets.only(right: 10),
-                        child: imgIcoOut),
-                    txtJK
-                  ]),
+      Positioned.fill(
+          child: imgKotakTgl),
+      Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+                margin: const EdgeInsets.only(right: 20), child: txtTanggal),
+            Container(
+              child: Column(children: <Widget>[
+                Row(children: <Widget>[
+                  Container(
+                      margin: const EdgeInsets.only(right: 10), child: imgIcoIn),
+                  txtJM
                 ]),
-              ),
-            ],
-          ),
+                Row(children: <Widget>[
+                  Container(
+                      margin: const EdgeInsets.only(right: 10), child: imgIcoOut),
+                  txtJK
+                ]),
+              ]),
+            ),
+          ],
         ),
       ),
-    ])
+    ]),
   ]),
 );
